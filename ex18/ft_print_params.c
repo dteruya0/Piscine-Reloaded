232798986d8087e7c1b652e63e5d02c5242a2808
Diego Teruya <dteruya@c2r6p4.42sp.org.br>
2024-10-08 13:03:42 -0300

    banana

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dteruya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 11:26:34 by dteruya           #+#    #+#             */
+/*   Updated: 2024/10/08 13:01:52 by dteruya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+/*
+{
+	write(1, &c, 1);
+}
+*/
+
+void	ft_print_params(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+		if (str[i] == '\0')
+		{
+			ft_putchar('\n');
+		}
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	value;
+
+	value = 1;
+	while (argv[value])
+	{
+		ft_print_params(argv[value]);
+		value++;
+	}
+	return (0);
+}
